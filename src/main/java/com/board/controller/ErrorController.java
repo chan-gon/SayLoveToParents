1e2d370f1b8f7304fcda1fed221acc7bc52b305e
Charlie <ruggedcafe@gmail.com>
2022-03-02 05:53:56 +0900

    Refactor: 에러 페이지 설정
    404, 500 에러에 대한 핸들링 페이지 작성

--- src/main/java/com/board/controller/ErrorController.java
+++ src/main/java/com/board/controller/ErrorController.java
@@ -1,17 +1,32 @@
 package com.board.controller;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import lombok.extern.log4j.Log4j;
 
-@ControllerAdvice
+/**
+ * @RestControllerAdvice
+ * : @ControllerAdvice + @ResponseBody
+ * 대상을 지정하지 않으면 모든 컨트롤러에 적용된다.
+ *
+ */
+@RestControllerAdvice
 @Log4j
 public class ErrorController {
 
 	@ExceptionHandler(Exception.class)
-	public String handleException(Exception ex) {
+	public ModelAndView handle500(Exception ex) {
 		log.info("Exception = " + ex.getMessage());
-		return "error/errorPage";
+		return new ModelAndView("error/error500");
 	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ModelAndView handle404(NoHandlerFoundException ex) {
+		log.info("Exception = " + ex.getMessage());
+		return new ModelAndView("error/error404");
+	}
+	
 }
