f363604b63a99be9661c34034644ffc3f18b62c9
Charlie <ruggedcafe@gmail.com>
2022-03-03 02:11:31 +0900

    Comment: 주석 변경
    주석 변경

--- src/main/java/com/board/controller/ErrorController.java
+++ src/main/java/com/board/controller/ErrorController.java
@@ -10,6 +10,7 @@ import lombok.extern.log4j.Log4j;
 /**
  * @RestControllerAdvice
  * : @ControllerAdvice + @ResponseBody
+ * 애플리케이션 내부에서 발생하는 에러를 제어하는 컨트롤러.
  * 대상을 지정하지 않으면 모든 컨트롤러에 적용된다.
  *
  */
@@ -19,13 +20,13 @@ public class ErrorController {
 
 	@ExceptionHandler(Exception.class)
 	public ModelAndView handle500(Exception ex) {
-		log.info("Exception = " + ex.getMessage());
+		log.info("Exception-500 = " + ex.getMessage());
 		return new ModelAndView("error/error500");
 	}
 	
 	@ExceptionHandler(NoHandlerFoundException.class)
 	public ModelAndView handle404(NoHandlerFoundException ex) {
-		log.info("Exception = " + ex.getMessage());
+		log.info("Exception-404 = " + ex.getMessage());
 		return new ModelAndView("error/error404");
 	}
 	
