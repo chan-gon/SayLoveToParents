efe8b4bce9e44eda5622e4ba445150d99103aadc
Charlie <ruggedcafe@gmail.com>
2022-03-07 00:14:06 +0900

    Style: 코드 포맷 수정

--- src/main/java/com/board/exception/user/UserExceptionHandler.java
+++ src/main/java/com/board/exception/user/UserExceptionHandler.java
@@ -4,19 +4,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.board.common.ApiResponse;
 import com.board.controller.UserController;
+import com.board.exception.common.ApiResponse;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * UserController에서 처리되는 모든 예외를 받아서 상세 내용을 출력한다.
+ *
+ */
 @Slf4j
 @RestControllerAdvice(basePackageClasses = { UserController.class })
 public class UserExceptionHandler {
 
 	@ExceptionHandler(UserException.class)
 	public ApiResponse userExceptionHandler(UserException e) {
+		log.warn("===== START =====");
 		log.warn("{}...", e.getClass(), e);
-
+		log.warn("===== END =====");
 		return new ApiResponse(HttpStatus.OK, e.getUserExceptionMessage().getMessage());
 	}
 }
--- src/main/java/com/board/exception/user/UserExceptionMessage.java
+++ src/main/java/com/board/exception/user/UserExceptionMessage.java
@@ -12,9 +12,10 @@ public enum UserExceptionMessage {
     UPDATE_FAIL ("사용자 수정에 실패했습니다."),
     DELETE_FAIL ("사용자 삭제에 실패했습니다."),
     PWD_UPDATE_FAIL ("비밀번호 변경에 실패했습니다."),
+    ALREADY_EXISTS ("이미 존재하는 사용자 입니다."),
 
 	;
 
-	private final String message;
+	private String message;
 	
 }
