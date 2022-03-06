136f1170c7d8ba437c42b76a1bf7fdd0a12b2ac0
Charlie <ruggedcafe@gmail.com>
2022-03-07 00:18:33 +0900

    Feat: 사용자 상품 찜한 여부 확인하는 로직 추가
    로그인한 사용자가 해당 상품을 찜했는지 아닌지 판단하는 로직 추가.

--- src/main/java/com/board/controller/ProductController.java
+++ src/main/java/com/board/controller/ProductController.java
@@ -1,10 +1,7 @@
 package com.board.controller;
 
-import java.security.Principal;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,11 +13,14 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.board.domain.ImageVO;
+import com.board.domain.ProductLikeVO;
 import com.board.domain.ProductVO;
 import com.board.domain.UserVO;
 import com.board.service.ImageService;
+import com.board.service.ProductLikeService;
 import com.board.service.ProductService;
 import com.board.service.UserService;
+import com.board.util.LoginUserUtils;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -37,6 +37,8 @@ public class ProductController {
 	
 	private final ImageService imageService;
 	
+	private final ProductLikeService productLikeService;
+	
 	/*
 	 * 비즈니스 로직
 	 */
@@ -47,29 +49,19 @@ public class ProductController {
 		productService.addNewProduct(user.getUserId(), product, productImage);
 	}
 	
-	@PostMapping(value = "/like/{prdtId}", produces = "application/text; charset=UTF-8")
-	public ResponseEntity<String> likeProduct(@PathVariable("prdtId") String prdtId) {
-		try {
-			productService.likeProuct(prdtId);
-		} catch (IllegalArgumentException e) {
-			return new ResponseEntity<String>("에러 발생. 다시 요청해주세요.", HttpStatus.CONFLICT);
-		}
-		return new ResponseEntity<String>("찜하기 완료.", HttpStatus.OK);
+	@PostMapping("/like/{prdtId}")
+	public void likeProduct(@PathVariable("prdtId") String prdtId) {
+		productService.likeProuct(prdtId);
 	}
 	
-	@PostMapping(value = "/unlike/{prdtId}", produces = "application/text; charset=UTF-8")
-	public ResponseEntity<String> unlikeProduct(@PathVariable("prdtId") String prdtId) {
-		try {
-			productService.unlikeProuct(prdtId);
-		} catch (IllegalArgumentException e) {
-			return new ResponseEntity<String>("에러 발생. 다시 요청해주세요.", HttpStatus.CONFLICT);
-		}
-		return new ResponseEntity<String>("찜하기 취소 완료.", HttpStatus.OK);
+	@PostMapping("/unlike/{prdtId}")
+	public void unlikeProduct(@PathVariable("prdtId") String prdtId) {
+		productService.unlikeProuct(prdtId);
 	}
 	
 	@PostMapping("/delete/{prdtId}")
-	public void deleteProduct(@PathVariable("prdtId") String prdtId, Principal principal) {
-		productService.deleteProduct(principal, prdtId);
+	public void deleteProduct(@PathVariable("prdtId") String prdtId) {
+		productService.deleteProduct(prdtId);
 	}
 	
 	/*
@@ -77,9 +69,9 @@ public class ProductController {
 	 */
 	
 	@GetMapping("/new")
-	public ModelAndView newProduct(Model model, Principal principal) {
+	public ModelAndView newProduct(Model model) {
 		try {
-			String username = principal.getName();
+			String username = LoginUserUtils.getUserId();
 			model.addAttribute("users", userService.getUserById(username));
 			return new ModelAndView("product/newProduct");
 		} catch (NullPointerException e) {
@@ -91,14 +83,16 @@ public class ProductController {
 	public ModelAndView getProduct(@PathVariable("prdtId") String prdtId, Model model) {
 		ProductVO selectedProduct = productService.getProductById(prdtId);
 		List<ImageVO> productImages = imageService.getImagesById(prdtId);
+		ProductLikeVO productLike = productLikeService.isLikedOrNot(prdtId);
+		model.addAttribute("productLike", productLike);
 		model.addAttribute("images", productImages);
 		model.addAttribute("product", selectedProduct);
 		return new ModelAndView("product/productDetail");
 	}
 	
 	@GetMapping("/shop")
-	public ModelAndView myShop(Principal principal, Model model) {
-		List<ProductVO> productList = productService.getProductListById(principal);
+	public ModelAndView myShop(Model model) {
+		List<ProductVO> productList = productService.getProductListById();
 		model.addAttribute("products", productList);
 		return new ModelAndView("product/shop");
 	}
